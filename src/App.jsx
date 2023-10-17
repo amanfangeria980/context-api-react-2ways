f35b1658919ffd846ff2c44d4be8cf8846752075
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-17 22:54:24 +0530

    added the login and profile component in our app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import UserContextProvider from './context/UserContextProvider';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
 
 function App() {
   return(
     <UserContextProvider>
     <h1>Context API with 2 Projects</h1>
+    <Login/>
+    <Profile/>
     </UserContextProvider>
   )
 }
