7bf973c2be40cb8c3b775229d61fcee3f89a3d5a
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-17 23:51:23 +0530

    wrapped our app components inside themeprovider

--- card-project/src/App.jsx
+++ card-project/src/App.jsx
@@ -1,7 +1,12 @@
 import ThemeBtn from "./components/ThemeBtn";
 import Card from "./components/Card";
+import {ThemeProvider} from "./context/ThemeContext"
 function App() {
+
+
   return (
+    <ThemeProvider>
+    
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -13,6 +18,7 @@ function App() {
         </div>
       </div>
     </div>
+    </ThemeProvider>
   );
 }
 
